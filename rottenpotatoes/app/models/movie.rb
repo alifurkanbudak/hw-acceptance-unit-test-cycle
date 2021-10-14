14f64405eac81991020c590cbacae059c7ceee6a
EC2 Default User <ec2-user@ip-172-31-17-101.us-east-2.compute.internal>
2021-10-14 21:58:54 +0000

    Complete

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+    def find_similar
+        Movie.where(director: self.director)
+    end
 end
--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,4 +1,6 @@
 Rottenpotatoes::Application.routes.draw do
+  get 'similar_movies/:id', to: 'similar_movies#index', as: 'similar_movies'
+
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
